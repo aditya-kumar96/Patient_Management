f57c67fd01982f52c2bd6282360ec03e4e0e2d8b
Aditya Kumar <aadikori9085@gmail.com>
2025-12-23 03:04:21 +0530

    create api and json data for the testing purpose

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+import json
 app = FastAPI()
+
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f) 
+    return data
 
 @app.get('/')
 def hello():
     return {'print':'hello'}
+
+@app.get("/view")
+def viewPatients():
+    data = load_data()
+    return data
+
